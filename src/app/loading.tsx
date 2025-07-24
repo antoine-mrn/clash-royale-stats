e689586d2e511c648d2ba22686786e456fb79c07
antoine-marion <antoine.marion.am@outlook.fr>
2025-07-24 15:40:39 +0200

    Loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Loading() {
+    return (
+        <div className="bg-base-100 h-full flex flex-1 flex-col items-center">
+            <Image
+                src="/waiting-emote.png"
+                alt="Waiting goblin illustration"
+                width={256}
+                height={200}
+                className="mt-[25%]"
+            />
+            <span className="loading loading-spinner loading-lg text-primary"></span>
+            <span className="text-sm text-neutral-content">
+                Loading your stats...
+            </span>
+        </div>
+    );
+}

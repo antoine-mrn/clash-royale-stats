d8bf2e846625bf606d254d709fde6c65bcb477bb
antoine-marion <antoine.marion.am@outlook.fr>
2025-08-26 13:11:15 +0200

    Find last finish season for homepage leaderboard

--- src/lib/serverMethod/playerRanking.ts
+++ src/lib/serverMethod/playerRanking.ts
@@ -1,10 +1,13 @@
 import { PlayerPreview } from "@/types/player.interface";
 import { fetchApi } from "../fetchApi";
+import { getLastSeasonId } from "./season";
 
 export async function getPlayerRanking(): Promise<PlayerPreview[]> {
     try {
+        const lastSeasonId = await getLastSeasonId();
+
         const response = await fetchApi(
-            `/locations/global/pathoflegend/2025-06/rankings/players?limit=10`
+            `/locations/global/pathoflegend/${lastSeasonId}/rankings/players?limit=10`
         );
 
         if (!response.ok) {
---
+++ src/lib/serverMethod/season.ts
@@ -0,0 +1,10 @@
+import { fetchApi } from "../fetchApi";
+
+export async function getLastSeasonId() {
+    const response = await fetchApi("/locations/global/seasons");
+    const { items } = await response.json();
+
+    return items.reduce((prev: { id: string }, curr: { id: string }) => {
+        return prev.id > curr.id ? prev : curr;
+    }).id;
+}

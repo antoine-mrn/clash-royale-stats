81ff61b8ad7f1c0121dabbd3c1a03502f183b068
antoine-marion <antoine.marion.am@outlook.fr>
2025-08-26 13:32:11 +0200

    util method to get new level of card

---
+++ src/utils/card.ts
@@ -0,0 +1,10 @@
+export const rarityOffset: Record<string, number> = {
+    common: 0,
+    rare: 2,
+    epic: 5,
+    legendary: 8,
+    champion: 10,
+};
+
+export const getNewLevel = (level: number, rarity: string): number =>
+    level + rarityOffset.rarity;

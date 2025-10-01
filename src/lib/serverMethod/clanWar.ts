552ba42ed9f984e11e0b6fd7d9259a958ed9d11b
antoine-mrn <antoine.marion.am@outlook.fr>
2025-10-01 20:38:50 +0200

    icon for river race page

--- src/lib/serverMethod/clanWar.ts
+++ src/lib/serverMethod/clanWar.ts
@@ -1,7 +1,10 @@
+import { CurrentRiverRace } from "@/types/riverRace.interface";
 import { fetchApi } from "../fetchApi";
 import mapCurrentRiverRace from "../mapper/mapWar";
 
-export async function getCurrentRiverRace(tag: string): Promise<any> {
+export async function getCurrentRiverRace(
+    tag: string
+): Promise<CurrentRiverRace> {
     try {
         const response = await fetchApi(`/clans/%23${tag}/currentriverrace`, {
             next: { revalidate: 600 },

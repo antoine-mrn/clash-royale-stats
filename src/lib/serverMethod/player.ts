e6fb53a7b115aa30c9ffb0a0041a4cd10ecbb540
antoine-mrn <antoine.marion.am@outlook.fr>
2026-05-06 11:53:26 +0200

    Testing unstable_cache

--- src/lib/serverMethod/player.ts
+++ src/lib/serverMethod/player.ts
@@ -2,15 +2,15 @@ import { Player, PlayerFromApi } from "@/types/player";
 import { fetchApi } from "../fetchApi";
 import { mapPlayer } from "../mapper/mapPlayer";
 import { cache } from "react";
+import { unstable_cache } from "next/cache";
 
-export const getPlayer = cache(async function getPlayer(
-    tag: string
-): Promise<Player> {
-    const response = await fetchApi(`/players/%23${tag}`, {
-        next: { revalidate: 300 },
-    });
+const fetchPlayer = unstable_cache(
+    async (tag: string): Promise<Player> => {
+        const res = await fetchApi(`/players/%23${tag}`);
+        return mapPlayer(await res.json());
+    },
+    ["player"],
+    { revalidate: 300 },
+);
 
-    const PlayerFromApi: PlayerFromApi = await response.json();
-
-    return mapPlayer(PlayerFromApi);
-});
+export const getPlayer = cache(fetchPlayer);
--- src/lib/serverMethod/playerBattlelog.ts
+++ src/lib/serverMethod/playerBattlelog.ts
@@ -1,14 +1,14 @@
 import { Battle, BattleFromApi } from "@/types/battle";
 import { fetchApi } from "../fetchApi";
 import { mapBattlelog } from "../mapper/mapBattlelog";
+import { unstable_cache } from "next/cache";
 
-export default async function getPlayerBattlelog(
-    tag: string
-): Promise<Battle[]> {
-    const response = await fetchApi(`/players/%23${tag}/battlelog`, {
-        next: { revalidate: 300 },
-    });
-
-    const battles: BattleFromApi[] = await response.json();
-    return battles.map((battle) => mapBattlelog(battle));
-}
+export const getPlayerBattlelog = unstable_cache(
+    async (tag: string): Promise<Battle[]> => {
+        const res = await fetchApi(`/players/%23${tag}/battlelog`);
+        const battles: BattleFromApi[] = await res.json();
+        return battles.map(mapBattlelog);
+    },
+    ["player-battlelog"],
+    { revalidate: 300 },
+);
